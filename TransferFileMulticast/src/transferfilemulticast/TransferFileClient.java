35544034e3c282d3f141b69123ad012154e94f7e
flaviolosada <usuario@192.168.25.224>
2017-03-17 00:57:04 -0300

    transferencia de arquivos necessario estudar como enviar um arquivo completo

--- TransferFileMulticast/src/transferfilemulticast/TransferFileClient.java
+++ TransferFileMulticast/src/transferfilemulticast/TransferFileClient.java
@@ -3,7 +3,6 @@ package transferfilemulticast;
 import java.io.*;
 import java.net.*;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 
 class TransferFileClient {
 	
@@ -14,42 +13,20 @@ class TransferFileClient {
 
 		int porta = 5000;
 		MulticastSocket clientSocket = new MulticastSocket();
-		InetAddress endereco = InetAddress.getByName("230.55.77.99");
+		InetAddress endereco = InetAddress.getByName("227.55.77.99");
 		clientSocket.joinGroup(endereco);
 		                
-                
-                File arquivo = abrirArquivo();
-                if (arquivo != null ) {
-                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(arquivo));
-
-                    bis.read(sendData, 0, sendData.length);
-                       
-                    sendData = "TEXT".getBytes();
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, endereco, porta);
-                    clientSocket.setTimeToLive(ttl);
-                    clientSocket.send(sendPacket);
-                    String serverName = receiveServerName();
-                    JOptionPane.showMessageDialog(null, serverName);
-    //		while (true) {
-    //			System.out.print ("Mensagem: ");
-    //			String sentence = stdIn.readLine();
-    //			if (sentence == null) break;
-    //			sendData = sentence.getBytes();
-    //			DatagramPacket sendPacket;
-    //			sendPacket = new DatagramPacket (sendData,
-    //					sendData.length, endereco, porta);
-    //			clientSocket.setTimeToLive (ttl);
-    //			clientSocket.send (sendPacket);
-    //			sentence = null;
-    //			sendPacket = null;
-    //		}
-
-                    clientSocket.leaveGroup(endereco);
-                    clientSocket.close();
-                }
+                sendData = "[TEXT]".getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, endereco, porta);
+                clientSocket.setTimeToLive(ttl);
+                clientSocket.send(sendPacket);
+                String serverName = receiveServerName();
+                clientSocket.leaveGroup(endereco);
+                clientSocket.close();
+                sendFileSocket(serverName);
 	}
         
-        private static File abrirArquivo() {
+        private static File openFile() {
             JFileChooser abreArquivo = new JFileChooser();
             abreArquivo.setAcceptAllFileFilterUsed(false);
             abreArquivo.setDialogType(JFileChooser.OPEN_DIALOG);
@@ -79,5 +56,25 @@ class TransferFileClient {
                 System.err.println(e.getMessage());
             }
             return "";
+        }
+        
+        private static void sendFileSocket(String ipServer) {
+            Socket socket;
+            byte[] sendData = new byte[1024];
+            try {                
+                /* Inicializacao dos fluxos de entrada e saida */
+                File arquivo = openFile();
+                if (arquivo != null ) {
+                    /* Inicializacao de socket TCP */
+                    socket = new Socket(ipServer, 1996);
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(arquivo));
+                    bis.read(sendData);
+                    socket.getOutputStream().write(sendData);
+                    bis.close();
+                    socket.close();
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
         }
 }
--- TransferFileMulticast/src/transferfilemulticast/TransferFileServer.java
+++ TransferFileMulticast/src/transferfilemulticast/TransferFileServer.java
@@ -5,18 +5,19 @@
  */
 package transferfilemulticast;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
@@ -27,10 +28,13 @@ public class TransferFileServer {
     /**
      * @param args the command line arguments
      */
+    
+    private static String ipCliente = "";
+    
     public static void main(String[] args) throws IOException {
-        String serverType = "TEXT";//args[0];
+        String serverType = "[TEXT]";//args[0];
         int porta = 5000;
-        String ip = "230.55.77.99";
+        String ip = "227.55.77.99";
         MulticastSocket socket = new MulticastSocket(porta);
         InetAddress endereco = InetAddress.getByName(ip);
         socket.joinGroup(endereco);
@@ -46,9 +50,17 @@ public class TransferFileServer {
 	      sentence = new String(recvPacket.getData());
 	      System.out.print(recvPacket.getAddress().toString() +": ");
               System.out.println (sentence);            
-              if (serverType.equals(sentence.trim())) {                 
-                sendMyName(recvPacket.getAddress().toString().substring(1));  
-                startSocket(recvPacket.getAddress().toString().substring(1));                      
+              if (serverType.equals(sentence.trim())) {        
+                ipCliente = recvPacket.getAddress().toString().substring(1);  
+                sendMyName(ipCliente); 
+                new Thread() {
+                    @Override
+                    public void run() {
+                        startSocket(ipCliente);
+                    }                    
+                }.start();
+              } else {
+                  ipCliente = "";
               }
 	      sentence= null;
 	      recvPacket = null;
@@ -66,21 +78,15 @@ public class TransferFileServer {
         try {
             /* Pegar parametros */
             String mensagemEnviar = "IP: " + InetAddress.getLocalHost().getHostAddress();
-
-        
             /* Inicializacao de sockets UDP com Datagrama */
             socket = new DatagramSocket();
-
             /* Configuracao a partir dos parametros */
             InetAddress host = InetAddress.getByName(ip);
             byte[] m = mensagemEnviar.getBytes();
-
             /* Criacao do Pacote Datagrama para Envio */
             request = new DatagramPacket(m, m.length, host, serverPort);
-
             /* Envio propriamente dito */
             socket.send(request);
-
             /* Preparacao do Pacote Datagrama para Recepcao */
             /* Finaliza tudo */
             socket.close ();
@@ -90,52 +96,29 @@ public class TransferFileServer {
     }
     
     public static void startSocket(String ip) {
-        Integer numeroPorta;
         ServerSocket serverSocket;
         Socket clientSocket;
-        PrintWriter out;
-        BufferedReader in;
-        String comando;
-
-        /* Parametros */
-        numeroPorta = 6000;
+        byte[] stream = new byte[1024];
 
         try {
             /* Inicializacao do server socket TCP */
-            serverSocket = new ServerSocket(numeroPorta);
-            while (true){
+            serverSocket = new ServerSocket(1996);            
                 /* Espera por um cliente */
-                clientSocket = serverSocket.accept();
-                System.out.println ("Novo cliente: "+serverSocket.toString());
-
-                /* Preparacao dos fluxos de entrada e saida */
-                out = new PrintWriter(clientSocket.getOutputStream(),
-                                true);
-                in = new BufferedReader(new InputStreamReader(
-                                clientSocket.getInputStream()));
-
-                /* Recuperacao dos comandos */
-                while ((comando = in.readLine()) != null) {	
-                        System.out.println ("Comando recebido: ["+ comando+"]");
-                        /* Se comando for "HORA" */
-                        if (comando.equals ("HORA")){
-                                /* Prepara a hora para envio */
-                                String hora = new SimpleDateFormat("d MMM yyyy HH:mm:ss").format(new Date ());
-                                /* Escreve na saida a 'hora' */
-                                out.println (hora);
-                        }else if (comando.equals ("FIM")){
-                                break;
-                        }else{
-                                out.println ("Comando Desconhecido");
-                        }
-                }
-                /* Finaliza tudo */
-                System.out.print ("Cliente desconectando... ");
-                out.close();
-                in.close();
-                clientSocket.close();
-                System.out.println ("ok");
-            }
+            clientSocket = serverSocket.accept();
+            /* Preparacao dos fluxos de entrada e saida */
+            BufferedInputStream bf = new BufferedInputStream(clientSocket.getInputStream());
+            bf.read(stream);
+            
+            Path file = Paths.get("C:\\Temp\\arquivo.txt");
+            
+            FileOutputStream fos = new FileOutputStream(file.toFile());
+            
+            fos.write(stream);
+            /* Finaliza tudo */
+            bf.close();
+            fos.flush();
+            fos.close();
+            clientSocket.close();
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
